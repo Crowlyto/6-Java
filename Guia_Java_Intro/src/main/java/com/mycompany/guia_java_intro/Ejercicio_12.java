59a48fac120e6e60506d59ca4ed71d30d1904dfa
Leandro Nuñez <112916360+Crowlyto@users.noreply.github.com>
2022-09-14 23:55:36 -0300

    Add files via upload

---
+++ Guia_Java_Intro/src/main/java/com/mycompany/guia_java_intro/Ejercicio_12.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.guia_java_intro;
+
+/**
+ *
+ * @author crowl
+ */
+public class Ejercicio_12 {
+
+    /**
+     * 
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ Guia_Java_Intro/src/main/java/com/mycompany/guia_java_intro/Ejercicio_15.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.guia_java_intro;
+
+/**
+ *
+ * @author crowl
+ */
+public class Ejercicio_15 {
+
+    /**
+     * Realizar un algoritmo que rellene un vector con los 100 primeros números enteros y los
+     * muestre por pantalla en orden descendente.
+     */
+    public static void main(String[] args) {
+        System.out.println(" ");
+    }
+    
+}
